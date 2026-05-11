9b13d9dfcda909320c8c8c5cb0e7807e12a5f6e4
Bruno <bruno_bsilva@yahoo.com.br>
2026-05-11 14:03:49 -0300

    Inclusion of initial project with user login and random password.

---
+++ src/test/java/test/bruno/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package test.bruno.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
